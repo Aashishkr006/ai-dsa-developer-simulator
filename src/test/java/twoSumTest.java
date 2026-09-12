bbb088e30b8878d6d7dc24ae1563205217bbc23b
Aashishkr006 <aashishkr006@gmail.com>
2026-09-12 23:50:01 +0530

    Add Two Sum

---
+++ src/test/java/twoSumTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    @Test
+    void testTwoSum() {
+
+        // Test will be written after the solution
+
+    }
+}
